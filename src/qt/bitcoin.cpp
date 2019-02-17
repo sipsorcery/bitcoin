486c3c3fd6a0325aa54d3ee377df556f6ff6468e
Aaron Clauson <aaron@sipsorcery.com>
2019-02-17 17:00:28 +0100

    WIP: bitcoin-qt builds but crashes.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -433,8 +433,8 @@ int GuiMain(int argc, char* argv[])
     // Do not refer to data directory yet, this can be overridden by Intro::pickDataDirectory
 
     /// 1. Basic Qt initialization (not dependent on parameters or configuration)
-    Q_INIT_RESOURCE(bitcoin);
-    Q_INIT_RESOURCE(bitcoin_locale);
+    //Q_INIT_RESOURCE(bitcoin);
+    //Q_INIT_RESOURCE(bitcoin_locale);
 
     BitcoinApplication app(*node, argc, argv);
     // Generate high-dpi pixmaps
